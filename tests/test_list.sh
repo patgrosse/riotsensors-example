1868f48e716f6c49ba1893336d8ce0a64007eab1
Patrick Grosse <patrick.grosse@uni-muenster.de>
2017-06-27 12:42:29 +0200

    Improved integration test

--- tests/test_list.sh
+++ tests/test_list.sh
@@ -7,13 +7,25 @@ echo "=========== Adding tap device"
 ip tuntap add tap0 mode tap
 ip link set tap0 up
 echo "=========== Starting term in background"
-((BOARD=native make term) | (sed 's/^/[BOARD OUTPUT] /')) &
+PID=`(./bin/native/rs-example.elf tap0 -d) | grep -Eo '[0-9]+'`
+echo Started with PID ${PID}
 sleep 2
 echo "=========== Print network configuration"
 ip a
 echo "=========== Calling CoAP client"
+krams=`coap-client 'coap://[fe80::e0e6:d4ff:fedf:3333%tap0]/list'`
+echo "CoAP output was:"
+echo $krams
 coap-client 'coap://[fe80::e0e6:d4ff:fedf:3333%tap0]/list' | grep -q -P '(?=.*?testint)(?=.*?testdouble)(?=.*?teststr)^.*$'
+val=$?
 echo "=========== Killing child processes"
-pkill -TERM -P ${$}
+kill -SIGTERM ${PID}
 echo "=========== Removing tap device"
 ip tuntap del tap0 mode tap
+
+if [ $? = 0 ]; then
+	echo "Success"
+else
+	echo "Test failed"
+	exit 1
+fi
